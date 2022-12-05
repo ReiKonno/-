df5bbcf771bf5d2ba69dcf1bbec8847b9d26db9a
EC2 Default User <ec2-user@ip-172-31-25-124.ap-northeast-1.compute.internal>
2022-12-05 17:05:45 +0000

    Ruby5[Finish]

---
+++ lesson5.rb
@@ -0,0 +1,13 @@
+total_price = 100
+
+if total_price > 100
+  puts "みかんを購入。所持金あり。"
+end
+
+if total_price == 100
+  puts "みかんを購入。所持金は０円。"
+end
+
+if total_price < 100
+  puts "みかんを購入することは出来ません。"
+end
